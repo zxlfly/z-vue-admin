f5041ef511f50cac5dd51fb8dd105a28b39f4b25
zxlfly <zxlfly@foxmail.com>
2024-08-04 15:44:03 +0800

    feat(mock): 调整mock

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,4 @@ const app = createApp(App)
 // for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 //     app.component(key, component)
 // }
-// 新增
-console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 app.mount("#app")
-// production mock server
-if (process.env.NODE_ENV === "production") {
-    import("./mockProdServer").then(({ setupProdMockServer }) => {
-        setupProdMockServer()
-    })
-}
--- vite-config/plugins/index.ts
+++ vite-config/plugins/index.ts
@@ -3,11 +3,13 @@ import vue from "@vitejs/plugin-vue"
 import useAutoImport from "./config/use-auto-import"
 import useComponents from "./config/use-components"
 import useIcons from "./config/use-icons"
+import useMock from "./config/use-mock"
 
 export default function getVitePlugins(): PluginOption[] {
     const plugins: PluginOption[] = [vue()]
     plugins.push(useAutoImport())
     plugins.push(useComponents())
     plugins.push(useIcons())
+    plugins.push(useMock())
     return plugins
 }
